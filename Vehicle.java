fd288b12f54a2a49a0497f3f4e21fbece2c314c9
Kavan Patel <kavanpatel1809@gmail.com>
2026-02-10 08:10:20 +0530

    Add files via upload

--- Vehicle.java
+++ Vehicle.java
@@ -1,98 +1,63 @@
+import java.time.Year;
+
 public class Vehicle {
-
-    // public members
-    public int vehicleId;
-    public String vehicleType;
-
-    // private members
-    private String vehicleName;
-    private String fuelType;
+    public String brand;
+    public String model;
+    public Year yearofMfg;
+    public String color;
+    public char fuelType;
+    public double price;
+    public int seats;
     private String mfgCode;
     private int noOfServices;
-    private double mileage;
 
-    // default constructor
-    public Vehicle() {
-        vehicleId = 0;
-        vehicleType = "NA";
-        vehicleName = "NA";
-        fuelType = "NA";
-        mfgCode = "NA";
-        noOfServices = 0;
-        mileage = 0;
-    }
-
-    // parameterized constructor
-    public Vehicle(int id, String type, String name, String fuel) {
-        vehicleId = id;
-        vehicleType = type;
-        vehicleName = name;
-        fuelType = fuel;
-        mfgCode = "NA";
-        noOfServices = 0;
-    }
-
-    // setters and getters
-    public void setVehicleName(String name) {
-        vehicleName = name;
-    }
-
-    public String getVehicleName() {
-        return vehicleName;
-    }
-
-    public void setFuelType(String fuel) {
-        fuelType = fuel;
-    }
-
-    public String getFuelType() {
-        return fuelType;
-    }
-
-    public void setMfgCode(String code) {
+    public void setMfgCode(String code){
         mfgCode = code;
     }
-
-    public String getMfgCode() {
+    public String getMfgCode(){
         return mfgCode;
     }
 
-    public void setNoOfServices(int services) {
-        noOfServices = services;
+    public void setNoOfServices(int nos){
+        noOfServices = nos;
     }
-
-    public int getNoOfServices() {
+    public int getNoOfServices(){
         return noOfServices;
     }
 
-    public double getMileage() {
-        return mileage;
+    public Vehicle(){
+        brand = "Toyota";
+        model = "Glanza";
+        yearofMfg = Year.of(2021);
+        color = "Red";
+        fuelType = 'P';
+        seats = 5;
+        price = 980000.00;
+        mfgCode = "TYT21GLZ";
+        noOfServices = 2;
     }
 
-    // vehicle actions
-    public void start() {
-        System.out.println(vehicleName + " started");
+    public Vehicle(String brand, String model, Year yearofMfg, String color, double price){
+        this.brand = brand;
+        this.model = model;
+        this.yearofMfg = yearofMfg;
+        this.color = color;
+        this.price = price;
     }
 
-    public void stop() {
-        System.out.println(vehicleName + " stopped");
+    public Vehicle(int seats, double price, char fuelType, String mfgCode, int noOfServices){
+        this.seats = seats;
+        this.price = price;
+        this.fuelType = fuelType;
+        this.mfgCode = mfgCode;
+        this.noOfServices = noOfServices;
     }
 
-    public void accelerate() {
-        System.out.println(vehicleName + " accelerating");
+    public int accelerate(int initSp){
+        return initSp + 20;
     }
 
-    // mileage calculation
-    public void calculateMileage(double distance, double fuelUsed) {
-        mileage = distance / fuelUsed;
-    }
-
-    // display method
-    public void displayDetails() {
-        System.out.printf(
-            "%-6d %-8s %-15s %-10s %-10s %-10d %-8.2f%n",
-            vehicleId, vehicleType, vehicleName, fuelType,
-            mfgCode, noOfServices, mileage
-        );
+    public float calcMileage(float fuelAmt, float dist){
+        return dist / fuelAmt;
     }
 }
